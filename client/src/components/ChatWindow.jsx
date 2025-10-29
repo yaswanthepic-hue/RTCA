c264fd392bc98feb8e0332526530d58f6da05cad
John Cena <sadhineniyaswanthkumar@gmail.com>
2025-10-29 05:55:25 +0530

    Remove pin button from chat window header

--- client/src/components/ChatWindow.jsx
+++ client/src/components/ChatWindow.jsx
@@ -420,12 +420,6 @@ const ChatWindow = ({ selectedUser, onBack }) => {
               <polyline points="21 15 16 10 5 21"/>
             </svg>
           </button>
-          <button onClick={handlePinChat} className={`action-btn ${isPinned ? 'active' : ''}`} title={isPinned ? 'Unpin Chat' : 'Pin Chat'}>
-            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-              <path d="M12 17v5"/>
-              <path d="M9 10.76a2 2 0 0 1-1.11 1.79l-1.78.9A2 2 0 0 0 5 15.24V16a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-.76a2 2 0 0 0-1.11-1.79l-1.78-.9A2 2 0 0 1 15 10.76V7a1 1 0 0 1 1-1 2 2 0 0 0 0-4H8a2 2 0 0 0 0 4 1 1 0 0 1 1 1z"/>
-            </svg>
-          </button>
         </div>
       </div>
 
